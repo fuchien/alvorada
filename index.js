ed9a5a5be2f2437c183b00fb667e71dbe955991f
Chiien <fuchienhsu@hotmail.com>
2017-07-20 12:08:13 -0300

    Add Bela Alvorada project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 4000
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+console.log(__dirname)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
+app.listen(port, () => {
+    console.log('Server running on port ' + port)
+})
