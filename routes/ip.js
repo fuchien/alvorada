2d68fc5ca3bcff36e1c89da8003db6ebca20180b
Chiien <fuchienhsu@hotmail.com>
2017-08-27 09:12:12 -0300

    Update IP router to GET

--- routes/ip.js
+++ routes/ip.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
 
     let resp = `<h1>Seu ip é ${req.headers['x-forwarded-for']}</h1>`
     res.send(resp);
